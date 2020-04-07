ebaa01be78eea8ccd57e7024df4ae9401649dc98
Srikar Prasad <srikasip@gmail.com>
2020-04-07 10:24:06 -0400

    Updated the firebase settings to be templatized

---
+++ AdaProjectSettings/firebaseSettings.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "YOUR API KEY",
+  authDomain: "YOUR AUTH DOMAIN",
+  databaseURL: "YOUR DATABASE URL",
+  projectId: "YOUR PROJECT ID",
+  storageBucket: "YOUR STORAGE BUCKET URL",
+  messagingSenderId: "YOUR MESSAGE SENDER ID",
+  appId: "YOUR APP ID",
+  measurementId: "YOUR MEASUREMENT ID",
+};
+
+export default firebaseConfig;
